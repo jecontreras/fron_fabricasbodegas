508538197ce4c0541bdf85f11a7ed5d2934592b4
jecontreras <joseeduar147@gmail.com>
2020-06-10 17:07:28 -0500

    cambios nuevos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: "https://mercadosback.herokuapp.com",
-  urlFront: "https://www.locomproaqui.com"
+  urlFront: "https://fabricaybodega-59c8e.web.app"
 };
